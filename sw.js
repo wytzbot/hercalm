f281122ce8a67e488979f825b8fd8886731dbbd9
wytzbot <ilemobayotolulope11092003@gmail.com>
2026-07-07 00:10:36 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='hercalm-v3'; // Changed to v3 to force update
-const FILES=['./','./index.html','./app.js','./manifest.json','./privacy.html'];
+const CACHE='hercalm-v3';
+const FILES=['./','./index.html','./app.js','./manifest.json','./privacy.html','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
