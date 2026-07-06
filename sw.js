694e66e263d112288d878664556feba058215ca1
wytzbot <ilemobayotolulope11092003@gmail.com>
2026-07-06 23:44:13 +0100

    Change cache version to v3 in service worker
    Updated cache version to v3 to force update.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='hercalm-v2';
+const CACHE='hercalm-v3'; // Changed to v3 to force update
 const FILES=['./','./index.html','./app.js','./manifest.json','./privacy.html'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
 self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
