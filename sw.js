df2ba0382a9a9ae19132b8ca2dfd66e13dd2ceb6
wytzbot <ilemobayotolulope11092003@gmail.com>
2026-07-09 09:01:45 +0100

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='hercalm-v3';
-const FILES=['./','./index.html','./app.js','./manifest.json','./privacy.html','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
